464292b17ea8d56ea1e3d9c4659d9ccee619aec4
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-08-18 20:56:48 +0200

    feat: add MenuType and initial menu items

---
+++ src/components/carte/index.ts
@@ -0,0 +1,18 @@
+export type MenuType = {
+    id: number;
+    name: string;
+    price: number;
+}
+
+export const menu: MenuType[] = [
+    {
+		id: 1,
+        name: "welsh",
+        price: 14		
+	},
+     {
+		id: 2,
+        name: "Steak",
+        price: 16		
+	},
+]
