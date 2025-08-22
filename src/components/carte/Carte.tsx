9db263a236bac0d363180b0255c90cb8e62a6ed3
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-08-22 17:04:51 +0200

    feat: add descrptif carte

--- src/components/carte/Carte.tsx
+++ src/components/carte/Carte.tsx
@@ -5,6 +5,11 @@ import style from "./carte.module.css"
 export default function Carte() {
     return(
         <section>
+            <section>
+            <h2>Notre carte</h2>
+            <p>Découvrez notre sélection de plats faits maison, préparés avec des ingrédients frais et de qualité.</p>
+            </section>
+
             <ul className={style.carte}>
                 {menu.map((m:MenuType)=>(
                     <li key={m.id} className={style.plat}>
