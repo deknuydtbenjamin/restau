90b5a67b45c25ea0d456872d31286778417ced39
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-08-22 16:46:31 +0200

    style:  align and center menu items

--- src/components/carte/Carte.tsx
+++ src/components/carte/Carte.tsx
@@ -1,14 +1,15 @@
 import type { MenuType } from "."
 import {menu} from "."
+import style from "./carte.module.css"
 
 export default function Carte() {
     return(
         <section>
-            <ul>
+            <ul className={style.carte}>
                 {menu.map((m:MenuType)=>(
-                    <li key={m.id}>
+                    <li key={m.id} className={style.plat}>
                         <h3>{m.name}</h3>
-                        <p>{m.price}</p>
+                        <p>{m.price} €</p>
                     </li>
                 ))}
             </ul>
