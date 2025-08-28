f71ddc15b39bb5e231d88e27599eb3993455e5ad
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-08-28 21:33:11 +0200

    feat: add allergene

--- src/components/carte/index.ts
+++ src/components/carte/index.ts
@@ -2,13 +2,20 @@ export type MenuType = {
     id: number;
     name: string;
     price: number;
+    allergene ?:string
 }
+
+// export type AllergeneType ={
+//     id:number;
+//     content: string;
+// }
 
 export const menu: MenuType[] = [
     {
 		id: 1,
         name: "welsh",
-        price: 14		
+        price: 14,
+        allergene:"Contient : gluten, lait, œuf, moutarde, sulfites"		
 	},
      {
 		id: 2,
@@ -16,3 +23,13 @@ export const menu: MenuType[] = [
         price: 16		
 	},
 ]
+// export const allergie :AllergeneType[] = [
+//     {
+//         id:1,
+//         content:"Contient : gluten, lait, œuf, moutarde, sulfites"
+//     },
+//     {
+//         id:2,
+//         content:"Contient "
+//     }
+// ]
