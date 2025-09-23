7392c5fbbe1b4e041428ac0b6b13d1f63e565eb5
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-09-23 10:56:41 +0200

    feat: add adress

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,6 +4,10 @@ export default function Footer() {
     return(
         <section>
             <h2>logo</h2>
+            <section>
+                <p>telephone: 00 00 00 00 00</p>
+                <p>adresse: 00 rue des rue à paris 00000 </p>
+            </section>
 
         </section>
     )
