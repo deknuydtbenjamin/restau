48689bd70f0d83127de1e05bee4ea68ecf508db9
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-09-29 12:00:34 +0200

    style: header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,10 @@ import style from "./header.module.css"
 
 export default function Header(){
     return(
-        <header>
+        <header className={style.header}>
             <p>Logo</p>
             <section>
-                <ul>
+                <ul className={style.menu}>
                     <li><a href="/">Accueil</a></li>
                     <li><a href="/">Carte</a></li>
                     <li><a href="/">Contact</a></li>
